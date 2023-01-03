aab619f68b98c535dca920cfab2c3a30ad35d066
fullyonline <no.reply4me@gmail.com>
2023-01-03 17:05:47 +0100

    Projektdateien hinzufügen.

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Materialverwaltung.Data;
+
+namespace Materialverwaltung
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
---
+++ Models/Material.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Materialverwaltung.Models
+{
+    public class Materialgruppe
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Material> Materials { get; set; }
+    }
+}
