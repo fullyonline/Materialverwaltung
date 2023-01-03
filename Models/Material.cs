abfca6835b1799dbf8f2fa3e5a34edfc0a2183f3
fullyonline <no.reply4me@gmail.com>
2023-01-03 18:12:27 +0100

    added the displaynames

--- Models/Material.cs
+++ Models/Material.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Materialverwaltung.Models
 {
@@ -8,10 +9,11 @@ namespace Materialverwaltung.Models
         public int Id { get; set; }
         [StringLength(255)]
         public string Name { get; set; }
+        [DisplayName("Lagerbestand")]
         public int Stock { get; set; }
-        [Range(0, 999999.99)]
+        [Range(0, 999999.99), DisplayName("Kaufspreis")]
         public decimal BuyPrice { get; set; }
-        [Range(0, 999999.99)]
+        [Range(0, 999999.99), DisplayName("Verkaufspreis")]
         public decimal SellPrice { get; set; }
 
         public virtual Materialgruppe Materialgruppe { get; set; }
