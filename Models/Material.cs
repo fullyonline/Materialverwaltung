5d4e61f4b9d935647b61ab3ad3d9efbe400c7d8a
fullyonline <no.reply4me@gmail.com>
2023-01-03 17:17:56 +0100

    switched classes in files

--- Models/Material.cs
+++ Models/Material.cs
@@ -2,14 +2,18 @@
 
 namespace Materialverwaltung.Models
 {
-    public class Materialgruppe
+    public class Material
     {
-
         [Key]
         public int Id { get; set; }
         [StringLength(255)]
         public string Name { get; set; }
+        public int Stock { get; set; }
+        [Range(0, 999999.99)]
+        public decimal BuyPrice { get; set; }
+        [Range(0, 999999.99)]
+        public decimal SellPrice { get; set; }
 
-        public virtual ICollection<Material> Materials { get; set; }
-    }
+        public virtual Materialgruppe Materialgruppe { get; set; }
+    }    
 }
--- Models/Materialgruppe.cs
+++ Models/Materialgruppe.cs
@@ -1,20 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace Materialverwaltung.Models
-{
-    public class Material
+{    public class Materialgruppe
     {
         [Key]
         public int Id { get; set; }
         [StringLength(255)]
         public string Name { get; set; }
-        public int Stock { get; set; }
-        [Range(0, 9999999999999999.99)]
-        public decimal BuyPrice { get; set; }
-        [Range(0, 9999999999999999.99)]
-        public decimal SellPrice { get; set; }
 
-
-        public virtual Materialgruppe Materialgruppe { get; set; }
+        public virtual ICollection<Material> Materials { get; set; }
     }
 }
